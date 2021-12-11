942e4e4a7f6eba232ed97731e49d95a2b7eef951
sl7389 <sl7389@nyu.edu>
2021-12-11 17:55:19 -0500

    added 2nd led

--- github-ledblnk.ino
+++ github-ledblnk.ino
@@ -1,14 +1,18 @@
-int ledPin=13;
+int ledPin = 13;
+int ledPin2 = 14;
 
 void setup() {
   // put your setup code here, to run once:
-pinMode(ledPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(ledPin,HIGH);
-delay(500);
-digitalWrite(ledPin,LOW);
-delay(500);
+  digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin2, LOW);
+  delay(500);
 }
